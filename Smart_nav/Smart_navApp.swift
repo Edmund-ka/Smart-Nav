e3310a509175c78fb8dbfaad36301341a046e7d5
Edmund <edmundka818@gmail.com>
2026-09-16 19:20:14 -0500

    Initial Commit

---
+++ Smart_nav/Smart_navApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_navApp.swift
+//  Smart_nav
+//
+//  Created by Edmund Afunyah on 9/16/26.
+//
+
+import SwiftUI
+
+@main
+struct Smart_navApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
